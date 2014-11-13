64e6fd3f9d5e253ea2e111fa333583c1434f8d9e
George Hodulik <ghodulik95@gmail.com>
2014-11-13 16:56:02 -0500

    Fixed some problems with BinomialFilter

--- BinomialFilter.java
+++ BinomialFilter.java
@@ -3,31 +3,25 @@ public class BinomialFilter extends Filter<Double>{
 	private FIRFilter bin;
 	
 	public BinomialFilter(int n){
-		double[] b = new double[n];
+		double[] b = new double[n+1];
 		setBinomial(b);
 		bin = new FIRFilter(b);
 	}
 	
 	private void setBinomial(double[] b) {
-		int n = b.length;
+		int n = b.length - 1;
 		int[] factorial = factorial(n);
-		for(int i = 0; i < n; i++){
-			b[i] = factorial[n - 1] / (factorial[i] * factorial[ n - i - 1]);
+		for(int i = 0; i <= n; i++){
+			b[i] = factorial[n] / (factorial[i] * factorial[ n - i]);
 		}
 	}
 	
 	private int[] factorial(int max){
-		int[] f = new int[max];
+		int[] f = new int[max+1];
 		f[0] = 1;
-		if(f.length > 1)
-			f[1] = 1;
-		else
-			return f;
-		
-		int currFactorial = 1;
-		for(int i = 2; i <= f.length; i++){
-			currFactorial = currFactorial * i;
-			f[i] = currFactorial;
+	
+		for(int i = 1; i <= max; i++){
+			f[i] = f[i-1]*i;
 		}
 		return f;
 	}
